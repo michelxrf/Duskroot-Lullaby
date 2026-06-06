7ab696e102bf4dfff2c85acf363de2c512ce4ef9
Daniel Neves <animael2006@gmail.com>
2026-06-06 00:17:25 -0300

    Pause and interactible itens UI

---
+++ Assets/!/Scripts/UI/Pause/PauseMenuManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using static Unity.Collections.Unicode;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private GameObject optionsCanvas;
+    [SerializeField] private Animator animator;
+
+    private bool paused;
+    private PlayerSetup localPlayer;
+    private PlayerControls controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+
+        controls.InGame.Pause.performed +=
+            OnPausePressed;
+    }
+
+    private void OnDisable()
+    {
+        controls.InGame.Pause.performed -=
+            OnPausePressed;
+    }
+
+    private void OnPausePressed(
+        InputAction.CallbackContext ctx)
+    {
+        if (paused)
+            Resume();
+        else
+            OpenPause();
+    }
+    public void OpenPause()
+    {
+        paused = true;
+
+        pauseCanvas.SetActive(true);
+
+        animator.ResetTrigger("Close");
+        animator.SetTrigger("Open");
+
+        localPlayer = FindLocalPlayer();
+
+        if (localPlayer != null)
+        {
+            localPlayer.EnablePlayerControlsLocal(false);
+        }
+        //pauseMenuScreen.SelectFirstButton();
+    }
+
+    public void Resume()
+    {
+        StartCoroutine(ClosePauseRoutine());
+    }
+    private IEnumerator ClosePauseRoutine()
+    {
+        paused = false;
+
+        animator.ResetTrigger("Open");
+        animator.SetTrigger("Close");
+
+        if (localPlayer != null)
+        {
+            localPlayer.EnablePlayerControlsLocal(true);
+        }
+
+        yield return new WaitForSeconds(0.4f);
+
+        pauseCanvas.SetActive(false);
+    }
+    private PlayerSetup FindLocalPlayer()
+    {
+        PlayerSetup[] players =
+            FindObjectsByType<PlayerSetup>(
+                FindObjectsSortMode.None);
+
+        foreach (PlayerSetup player in players)
+        {
+            if (player.IsLocalPlayer())
+                return player;
+        }
+
+        return null;
+    }
+    public void OpenOptions()
+    {
+        optionsCanvas.SetActive(true);
+    }
+    public async void BackToMenu()
+    {
+        if (RunnerBootstrap.Instance != null)
+        {
+            await RunnerBootstrap.Instance.Runner.Shutdown();
+        }
+
+        SceneManager.LoadScene("MainMenu1");
+    }
+
+
+}
