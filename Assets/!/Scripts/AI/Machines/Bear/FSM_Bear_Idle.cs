cefe3de76113058754fe74d5ac2bd8ace8547c9b
Michel <michelxrf@gmail.com>
2026-06-08 18:01:03 -0300

    New characters
    - updated Antonio and Alice models to new version made by Barbara. Had to make a special character setup to re-scale the character rig and enable it's box hat because he's bigger than the other models
    - added the Bear enemy character with it's FSM, almost the same as the mosquito with one more state

---
+++ Assets/!/Scripts/AI/Machines/Bear/FSM_Bear_Idle.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FSM_Bear_Idle : State
+{
+    [SerializeField] float idleDuration = 5f;
+    [SerializeField] State stateOnPlayerFound;
+    [SerializeField] State stateOnEndTimer;
+
+    Animator animator;
+    VisionCollider visionCollider;
+    NavMeshAgent navAgent;
+    EnemySetup enemySetup;
+    EnemyData enemyData;
+
+    float timeSpent = 0f;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        visionCollider = GetComponentInChildren<VisionCollider>();
+        animator = GetComponentInChildren<Animator>();
+        stateMachine = GetComponent<StateMachine>();
+        enemySetup = GetComponent<EnemySetup>();
+    }
+    public override void Enter()
+    {
+        navAgent.isStopped = true;
+        navAgent.speed = 0f;
+        timeSpent = 0f;
+    }
+
+    public override void Exit()
+    {
+        
+    }
+
+    public override void Process()
+    {
+        timeSpent += Time.deltaTime;
+        if (timeSpent >= idleDuration)
+        {
+            stateMachine.ChangeState(stateOnEndTimer);
+        }
+
+        animator.SetFloat("Speed", navAgent.velocity.magnitude);
+    }
+}
---
+++ Assets/!/Scripts/AI/Machines/Bear/FSM_Bear_Patrol.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FSM_Bear_Patrol : State
+{
+    [SerializeField] Transform patrolAnchor;
+    [SerializeField] float patrolRange = 5f;
+    [SerializeField] float stoppingDistance = .1f;
+    [SerializeField] float rotationSpeed = 60f;
+    [SerializeField] State stateOnPlayerFound;
+    [SerializeField] State stateOnPatrolPointReached;
+
+    Animator animator;
+    VisionCollider visionCollider;
+    NavMeshAgent navAgent;
+    EnemySetup enemySetup;
+    EnemyData enemyData;
+
+    Vector3 patrolDestination;
+    float distanceToDestination;
+
+    public override void Spawned()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        visionCollider = GetComponentInChildren<VisionCollider>();
+        animator = GetComponentInChildren<Animator>();
+        stateMachine = GetComponent<StateMachine>();
+        enemySetup = GetComponent<EnemySetup>();
+    }
+
+    public override void Enter()
+    {
+        if (!enemySetup.IsInitialized())
+        {
+            enemySetup.OnInit += () =>
+            {
+                enemyData = enemySetup.GetEnemyData();
+                navAgent.speed = enemyData.speed / 2f;
+            };
+        }
+        else
+        {
+            enemyData = enemySetup.GetEnemyData();
+            navAgent.speed = enemyData.speed / 2f;
+        }
+
+        patrolDestination = GetNewPatrolDestination();
+
+        navAgent.isStopped = false;
+        
+        navAgent.stoppingDistance = stoppingDistance;
+        navAgent.SetDestination(patrolDestination);
+
+        visionCollider.OnPlayerEntered += PlayerFound;
+    }
+
+    public override void Exit()
+    {
+    }
+
+    void RotateTowardPathNode()
+    {
+        if (navAgent.hasPath && navAgent.path.corners.Length > 1)
+        {
+            Vector3 directionToNextCorner = (navAgent.path.corners[1] - transform.position).normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(directionToNextCorner);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    public override void Process()
+    {
+        if (!HasStateAuthority) return;
+
+        if (IsDestinationReached())
+        {
+            stateMachine.ChangeState(stateOnPatrolPointReached);
+        }
+
+        RotateTowardPathNode();
+        animator.SetFloat("Speed", navAgent.velocity.magnitude);
+    }
+
+    Vector3 GetNewPatrolDestination()
+    {
+        if (patrolAnchor == null)
+        {
+            patrolAnchor = transform.parent;
+            if (patrolAnchor == null)
+            {
+                GameObject anchorObj = new GameObject(gameObject.name + "_PatrolAnchor");
+                anchorObj.transform.position = transform.position;
+                patrolAnchor = anchorObj.transform;
+
+                Debug.LogWarning("Patrol anchor not set for " + gameObject.name + ". Defaulting to current position.");
+            }
+        }
+
+        Vector2 randomPoint = Random.insideUnitCircle * patrolRange;
+        Vector3 destination = patrolAnchor.position + new Vector3(randomPoint.x, 0, randomPoint.y);
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(destination, out hit, patrolRange, NavMesh.AllAreas))
+        {
+            return hit.position;
+        }
+        return patrolAnchor.position; // Fallback to center if no valid point found
+    }
+
+    bool IsDestinationReached()
+    {
+        distanceToDestination = (patrolDestination - transform.position).magnitude;
+        return (distanceToDestination < stoppingDistance);
+    }
+
+    void PlayerFound(Transform playerTransf)
+    {
+        stateMachine.ChangeState(stateOnPlayerFound);
+    }
+
+    private void OnDisable()
+    {
+
+        if (!HasStateAuthority) return;
+
+        visionCollider.OnPlayerEntered -= PlayerFound;
+    }
+}
