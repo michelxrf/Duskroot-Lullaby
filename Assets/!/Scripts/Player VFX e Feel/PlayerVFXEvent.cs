bcd632d3414f9cc9d020d2ab0e4beb74e748a5cf
Daniel Neves <animael2006@gmail.com>
2026-06-08 11:53:40 -0300

    Start Player feels and MainMenu min char in TMP_InputFuild

---
+++ Assets/!/Scripts/Player VFX e Feel/PlayerVFXEvent.cs
@@ -0,0 +1,20 @@
+public enum PlayerVFXEvent
+{
+    Dash,
+    RunStart,
+    Heal,
+    Hit,
+    Death,
+
+    SwordStartAtk,
+    SwordAttackHit,
+
+    AxeStartAtk,
+    AxeAttackHit,
+
+    slingshotStartAtk,
+    slingshotAttackHit,
+
+    StaffStartAtk,
+    StaffAttackHit
+}
