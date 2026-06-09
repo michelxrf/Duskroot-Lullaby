a3c4c91f49e48fe49d8da78569e8b05f9e1b7f45
Michel <michelxrf@gmail.com>
2026-06-08 22:19:20 -0300

    Ghost and bear finshed

---
+++ Assets/!/Scripts/AI/Machines/Ghost/FSM_Ghost_Idle.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FSM_Ghost_Idle : State
+{
+    [SerializeField] float idleDuration = 5f;
+    [SerializeField] State stateOnPlayerFound;
+    [SerializeField] State stateOnEndTimer;
+
+    VisionCollider visionCollider;
+    NavMeshAgent navAgent;
+    EnemySetup enemySetup;
+    EnemyData enemyData;
+
+    float timeSpent = 0f;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        visionCollider = GetComponentInChildren<VisionCollider>();
+        stateMachine = GetComponent<StateMachine>();
+        enemySetup = GetComponent<EnemySetup>();
+    }
+    public override void Enter()
+    {
+        navAgent.isStopped = true;
+        navAgent.speed = 0f;
+        timeSpent = 0f;
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void Process()
+    {
+        timeSpent += Time.deltaTime;
+        if (timeSpent >= idleDuration)
+        {
+            stateMachine.ChangeState(stateOnEndTimer);
+        }
+    }
+}
